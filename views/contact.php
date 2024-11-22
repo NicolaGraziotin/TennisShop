4bd21032553748606aca53c4184b7ea84fdd4a7d
Galli Mattia <mattia.galli9@studio.unibo.it>
2024-11-22 12:58:01 +0100

    Changed contact page

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,22 @@
-<h1>Contact</h1>
+<!-- Header-->
+<header class="bg bg-no-repeat py-5" style="background-image: url(https://miro.medium.com/v2/resize:fit:1400/1*lVtPxJC36UaGvaKJXSr7zg.jpeg);">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Contact</h1>
+        </div>
+    </div>
+</header>
+<!-- Section-->
+<section class="company-info">
+    <h2>Info</h2>
+    <p><strong>Nome Azienda:</strong> Tennis Shop</p>
+    <p><strong>Indirizzo:</strong> Via Esempio 123, Cesena, Italia</p>
+    <p><strong>Email:</strong> tennisShop@unibo.it</p>
+    <p><strong>Telefono:</strong> +39 123 456 789</p>
+    <p>Segui i nostri social media per rimanere aggiornato!</p>
+    <ul class="social-links">
+        <li><a class="bi bi-facebook" href="https://facebook.com"></a></li>
+        <li><a class="bi bi-instagram" href="https://instagram.com"></a></li>
+        <li><a class="bi bi-twitter" href="https://twitter.com"></a></li>
+    </ul>
+</section>
