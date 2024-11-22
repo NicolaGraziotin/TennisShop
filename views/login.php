99ab9cd5c7a4db4bf4a128927d3b77e3d8f6d255
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2024-11-22 14:37:53 +0100

    Modified login form

--- views/login.php
+++ views/login.php
@@ -1,9 +1,18 @@
-<h1>Login</h1>
-
-<form action="" method="post">
-    <label for="email">Email</label>
-    <input type="text" name="email" id="email" required>
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" required>
-    <button type="submit">Login</button>
-</form>
+<section class="p-5">
+    <h1>Login</h1>
+    <form action="/login" method="post">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="seller" value="0">
+        <label class="form-check-label" for="seller">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+</section>
